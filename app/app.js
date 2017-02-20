190b3bd5aa0209681dbde9e7f49d116c76a039e4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-20 17:11:37 +0100

    add: myBody.html, myFooter, myHeader.html

--- app/app.js
+++ app/app.js
@@ -1,4 +1,25 @@
-(function() {
+(function () {
     'use strict';
-    
+
+    var app = angular.module('main', []);
+
+    app.directive('myHeader', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'tmpl/myHeader.html'
+        };
+    });
+    app.directive('myBody', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'tmpl/myBody.html'
+        };
+    });
+    app.directive('myFooter', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'tmpl/myFooter.html'
+        };
+    });
+
 })();
