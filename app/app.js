e8cc48f8f74345c87fd6737aee3f41a3d584e66d
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-21 15:34:51 +0100

    add: colorisation du menu

--- app/app.js
+++ app/app.js
@@ -4,6 +4,14 @@
     var app = angular.module('main', ['tt-route']);
 
 
+    app.run(['$rootScope', '$location', function($rootScope, $location) {
+        $rootScope.isActive = function(url) {
+            var path = $location.path();
+            //console.log('path', path);
+            return path === url;
+        };
+    }]);
+
     app.directive('myHeader', function() {
         return {
             restrict: 'E',
