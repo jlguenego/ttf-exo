b739226a960c8506644a408882456cc75c1763a4
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-21 09:43:03 +0100

    add:eslint

--- app/app.js
+++ app/app.js
@@ -1,21 +1,21 @@
-(function () {
+(function() {
     'use strict';
 
     var app = angular.module('main', []);
 
-    app.directive('myHeader', function () {
+    app.directive('myHeader', function() {
         return {
             restrict: 'E',
             templateUrl: 'tmpl/myHeader.html'
         };
     });
-    app.directive('myBody', function () {
+    app.directive('myBody', function() {
         return {
             restrict: 'E',
             templateUrl: 'tmpl/myBody.html'
         };
     });
-    app.directive('myFooter', function () {
+    app.directive('myFooter', function() {
         return {
             restrict: 'E',
             templateUrl: 'tmpl/myFooter.html'
