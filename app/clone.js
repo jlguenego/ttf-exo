7f6bf9b968fcefd20c968bc72893839f2fd776a0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-21 15:01:09 +0100

    add : angular-route

--- app/clone.js
+++ app/clone.js
@@ -3,20 +3,26 @@
 
     function clone(obj) {
         var result = {};
+        
         for (var p in obj) {
-            if (typeof obj[p] === 'object' ) {
+            if (typeof obj[p] === 'object' && obj[p] !== null) {
                 result[p] = clone(obj[p]);
             } else {
                 result[p] = obj[p];
             }
         }
+        // result.prototype = Object.create(obj.prototype);
         return result;
     }
 
-    var a = {x: {y: 4}, z: 'coucou'};
+    var a = {x: {y: 4}, z: 'coucou', a: null};
     var b = clone(a);
     b.x.y = 5;
     b.z = 'salut';
+    console.log('a', a);
+    console.log('b', b);
+    a = [3, 6, 'kiki'];
+    b = clone(a);
     console.log('a', a);
     console.log('b', b);
 })();
