9e259a5f2f770c41f07f0051caf8d9dd929426ea
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-22 14:01:24 +0100

    add:s2-3-4-5

--- app/tt-route/tt-route.js
+++ app/tt-route/tt-route.js
@@ -34,12 +34,22 @@ app.config(function($routeProvider, $locationProvider) {
 
 app.controller('ProductCtrl', function ProductCtrl($scope, $http, $rootScope, $q) {
 	'ngInject';
+	$scope.messages = [];
 	$q.when('start').then(function() {
 		$rootScope.showSpinner = true;
 		return $http.get('/ws/s1');
 	}).then(function(response) {
-		console.log('response', response);
-		$scope.affichage = response.data;
+		console.log('response s1', response);
+		$scope.messages.push(response.data);
+		return $q.all([$http.get('/ws/s2'), $http.get('/ws/s3'), $http.get('/ws/s4')]);
+	}).then(function(responses) {
+		console.log('responses s2-3-4', responses);
+		var datas = responses.map((n) => n.data);
+		Array.prototype.push.apply($scope.messages, datas);
+		return $http.get('/ws/s5');
+	}).then(function(response) {
+		console.log('response s5', response);
+		$scope.messages.push(response.data);
 	}).finally(function() {
 		$rootScope.showSpinner = false;
 	}).catch(function(error) {
--- webservices.js
+++ webservices.js
@@ -11,6 +11,26 @@ router.use(function(req, res, next) {
 
 router.get('/s1', function(req, res) {
 	setTimeout(function() {
-		res.json({result: 's1dddd', titi: 'dqsdqsdqsd'});
+		res.json({result: 's1', titi: 'dqsdqsdqsd'});
+	}, 2000);
+});
+router.get('/s2', function(req, res) {
+	setTimeout(function() {
+		res.json({result: 's2', titi: 'dqsdqsdqsd'});
+	}, 1000);
+});
+router.get('/s3', function(req, res) {
+	setTimeout(function() {
+		res.json({result: 's3', titi: 'dqsdqsdqsd'});
+	}, 2000);
+});
+router.get('/s4', function(req, res) {
+	setTimeout(function() {
+		res.json({result: 's4', titi: 'dqsdqsdqsd'});
+	}, 3000);
+});
+router.get('/s5', function(req, res) {
+	setTimeout(function() {
+		res.json({result: 's5', titi: 'dqsdqsdqsd'});
 	}, 2000);
 });
