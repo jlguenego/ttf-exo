49b7b17171ea049104f4755a8173ca057b3905f0
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-02-22 16:29:08 +0100

    add: star

--- app/tt-star/tt-star.js
+++ app/tt-star/tt-star.js
@@ -4,21 +4,26 @@ var app = angular.module('tt-star', []);
 
 app.directive('ttStar', function() {
     return {
+        scope: {
+            n: '=note'
+        },
         link: function(scope, element, attr) {
             console.log('tt-star link', arguments);
-            var html = '';
-            var note = attr.note;
-            note = (note === undefined) ? 0: note;
-            note = (isNaN(note)) ? 0: note;
-            note = (note > 5) ? 5: note;
-            note = (note < 0) ? 0: note;
-            for(var i = 0; i < note; i++) {
-                html += '<img src="./tt-star/img/yellow_star.png" />';
-            }
-            for(var i = note; i < 5; i++) {
-                html += '<img src="./tt-star/img/white_star.png" />';
-            }
-            element.html(html);
+            scope.$watch('n', function() {
+                var html = '';
+                var note = scope.n;
+                note = (note === undefined) ? 0: note;
+                note = (isNaN(note)) ? 0: note;
+                note = (note > 5) ? 5: note;
+                note = (note < 0) ? 0: note;
+                for(var i = 0; i < note; i++) {
+                    html += '<img src="./tt-star/img/yellow_star.png" />';
+                }
+                for(var i = note; i < 5; i++) {
+                    html += '<img src="./tt-star/img/white_star.png" />';
+                }
+                element.html(html);
+            });
         }
     };
 });
